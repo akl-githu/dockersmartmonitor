437a64d71d6c1881df6354d4bb57db77029b4a1b
akl-githu <mohamedakl082@gmail.com>
2025-07-15 17:10:50 +0400

    Finalizing the front end web pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,4 +28,22 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult RedirectPage(string userType)
+    {
+        if (userType == "admin")
+        {
+            return RedirectToAction("AdminPage");
+        }
+        else if (userType == "viewer")
+        {
+            return RedirectToAction("ViewerPage");
+        }
+        return RedirectToAction("SignIn");
+    }
+
+    public IActionResult SignIn()
+    {
+        return View();
+    }
 }
--- obj/Debug/net8.0/smartmonitor.AssemblyInfo.cs
+++ obj/Debug/net8.0/smartmonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("smartmonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffeb96222b7fe3b9e3f3262cdd953e1885874fee")]
 [assembly: System.Reflection.AssemblyProductAttribute("smartmonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("smartmonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
